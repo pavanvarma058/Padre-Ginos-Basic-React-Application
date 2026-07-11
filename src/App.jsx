31d2543474eed27211a6646f598bf580d6b35dcb
Pavan Varma <pavanvarma058@gmail.com>
2026-07-11 15:48:36 +0100

    Replaced the Pizza component with Order Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order now</h1>
-      <Pizza name="Pepperoni" description="Classic pizza with tomato sauce, mozzarella cheese, and fresh basil." image={"../api/public/pizzas/pepperoni.webp"}/>
-      <Pizza name="Hawaiian" description="A popular pizza topped with tomato sauce, mozzarella cheese, and pepperoni slices." image={"../api/public/pizzas/hawaiian.webp"} />
-      <Pizza name="Americano" description="A delicious pizza loaded with a variety of fresh vegetables, including bell peppers, onions, mushrooms, and olives." image={"../api/public/pizzas/big_meat.webp"} /> 
+      {/* <Pizza
+        name="Pepperoni"
+        description="Classic pizza with tomato sauce, mozzarella cheese, and fresh basil."
+        image={"../api/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Hawaiian"
+        description="A popular pizza topped with tomato sauce, mozzarella cheese, and pepperoni slices."
+        image={"../api/public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="Americano"
+        description="A delicious pizza loaded with a variety of fresh vegetables, including bell peppers, onions, mushrooms, and olives."
+        image={"../api/public/pizzas/big_meat.webp"}
+      /> */}
+      <Order />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
